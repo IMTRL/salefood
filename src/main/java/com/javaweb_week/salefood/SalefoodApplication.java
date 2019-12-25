486242da45956cffbd7b46f39ffdea8f2041bd0c
IMTRL <2691943504@qq.com>
2019-12-25 13:43:57 +0800

    Initial commit

---
+++ src/main/java/com/javaweb_week/salefood/SalefoodApplication.java
@@ -0,0 +1,13 @@
+package com.javaweb_week.salefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalefoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalefoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaweb_week/salefood/SalefoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb_week.salefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalefoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
