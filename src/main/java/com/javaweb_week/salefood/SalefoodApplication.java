75c22685a6c498efe3bdf1e38458d71bcf9dab72
IMTRL <2691943504@qq.com>
2019-12-25 16:51:57 +0800

    0.0.1

--- src/main/java/com/javaweb_week/salefood/SalefoodApplication.java
+++ src/main/java/com/javaweb_week/salefood/SalefoodApplication.java
@@ -1,9 +1,11 @@
 package com.javaweb_week.salefood;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.javaweb_week.salefood.dao")
 public class SalefoodApplication {
 
     public static void main(String[] args) {
