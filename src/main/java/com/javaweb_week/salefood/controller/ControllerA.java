00e6dcb2903deec9daad12c5c0b0c52ecd147624
IMTRL <2691943504@qq.com>
2019-12-26 16:18:41 +0800

    0.0.1

--- src/main/java/com/javaweb_week/salefood/controller/ControllerA.java
+++ src/main/java/com/javaweb_week/salefood/controller/ControllerA.java
@@ -7,37 +7,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ControllerA {
 
     @RequestMapping("/student-mainpage")
-    public String studentMainpage(){
+    public String studentMainpage() {
         return "Student_function";
     }
 
     @RequestMapping("/student-login")
-    public String studentLogin(){
+    public String studentLogin() {
         return "Student_login";
     }
 
     @RequestMapping("/student-regist")
-    public String studentRegist(){
+    public String studentRegist() {
         return "Student_regist";
     }
 
     @RequestMapping("/student-registing")
-    public String studentRegisting(){
+    public String studentRegisting() {
         return "Student_regist";
     }
 
     @RequestMapping("/student-info")
-    public String studentInfo(){
+    public String studentInfo() {
         return "Student_info";
     }
 
     @RequestMapping("/student-buyfood")
-    public String studentBuyfood(){
+    public String studentBuyfood() {
         return "Student_buyfood";
     }
 
     @RequestMapping("/student-orderlist")
-    public String studentOrderlist(){
+    public String studentOrderlist() {
         return "Student_orderlist";
+    }
+
+    @RequestMapping("/student-password-update")
+    public String studentPasswordPpdate() {
+        return "Student_password_update";
     }
 }
