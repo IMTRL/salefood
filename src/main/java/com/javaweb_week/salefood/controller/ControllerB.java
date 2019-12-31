a0ad0bfd835c9364177849c37f703e0fdb82a4b0
Yqs817 <473859833@qq.com>
2019-12-31 22:26:24 +0800

    食堂管理员功能完成

--- src/main/java/com/javaweb_week/salefood/controller/ControllerB.java
+++ src/main/java/com/javaweb_week/salefood/controller/ControllerB.java
@@ -6,17 +6,20 @@ import com.javaweb_week.salefood.entity.Rstmanager;
 import com.javaweb_week.salefood.service.FoodsService;
 import com.javaweb_week.salefood.service.MeatService;
 import com.javaweb_week.salefood.service.RstmanagerService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
+
 
 @Controller
 public class ControllerB {
@@ -32,16 +35,15 @@ public class ControllerB {
     //localhost：8080/RstManager_Login
     //食堂管理员登录
     @RequestMapping("/RstManager_Login")//action="RstManager_Login"
-    public String RstManagerLogin(String rmusername, String rmpassword, Map<String,Rstmanager> map){
-        Rstmanager rstManager = rstmanagerService.RstManagerLogin(rmusername,rmpassword);
-        if(rstManager != null){//判断输入的账号和密码是正确
-            map.put("rstManager",rstManager);
+    public String RstManagerLogin(String rmusername, String rmpassword, Map<String, Rstmanager> map) {
+        Rstmanager rstManager = rstmanagerService.RstManagerLogin(rmusername, rmpassword);
+        if (rstManager != null) {//判断输入的账号和密码是正确
+            map.put("rstManager", rstManager);
             return "RstManager_main";
-        }else{
+        } else {
             return "RstManager_Login";
         }
     }
-
 
 
     //返回主页面
@@ -53,83 +55,136 @@ public class ControllerB {
 
     //获取菜组信息(相当于总表详情)
     @RequestMapping("/get_Foods")
-    public String getFoods(Map<String,Object> map){
+    public String getFoods(Map<String, Object> map) {
         List<Foods> foods = foodsService.selectFoods();
-        map.put("foods",foods);
+        map.put("foods", foods);
         return "Foods";//Foods.html
     }
+
     //进入添加菜组页面（菜组表）
-    @GetMapping(value="/add_Foods")
-    public String addFoods(){
+    @GetMapping(value = "/add_Foods")
+    public String addFoods() {
         return "add_Foods";
     }
+
     //实际完成添加业务
-    @PostMapping(value="/add_Foods")
-    public String addFoods(Foods foods){
+    @PostMapping(value = "/add_Foods")
+    public String addFoods(Foods foods) {
         foodsService.insertFoods(foods);
         return "RstManager_main";
     }
 
 
     //修改的预处理，通过菜组编号Fid获取当前记录
-    @RequestMapping(value="f_update",method = RequestMethod.GET)
-    public String updateFoodsPre(int Fid,Model model){
+    @RequestMapping(value = "f_update", method = RequestMethod.GET)
+    public String updateFoodsPre(int Fid, Model model) {
         Foods foods = foodsService.selectFoodsById(Fid);
-        model.addAttribute("foods",foods);
+        model.addAttribute("foods", foods);
         return "update_Foods";//进入修改页面
     }
+
     //实际的修改
-    @RequestMapping(value="/update_Foods",method = RequestMethod.POST)
-    public String updateFoods(Foods foods){
+    @RequestMapping(value = "/update_Foods", method = RequestMethod.POST)
+    public String updateFoods(Foods foods) {
         foodsService.updateFoodsById(foods);
         return "RstManager_main";
     }
 
     //Foods表，删除完成后 需要重新获取删除后的新的数据，再显示
     @RequestMapping("/f_delete")
-    public String deleteFoods(int Fid){
+    public String deleteFoods(int Fid) {
         foodsService.deleteFoodsById(Fid);
         return "RstManager_main";
     }
 
     //显示对应菜组所有的菜品
     @RequestMapping("/f_meat")
-    public String getMeat(Integer Fid,Map<String,Object> map){
+    public String getMeat(Integer Fid, Map<String, Object> map) {
         List<Meat> meat = meatService.selectMeat(Fid);
-        map.put("meat",meat);
+        map.put("meat", meat);
         return "Meat";//Meat.html
     }
 
     //进入添加菜品页面（菜品表Meat）
-    @GetMapping(value="/add_Meat")
-    public String addMeat(){
+    @GetMapping(value = "/add_Meat")
+    public String addMeat() {
         return "add_Meat";
     }
+
+    //private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ControllerB.class);
+
     //实际完成添加业务
-    @PostMapping(value="/add_Meat")
-    public String addMeat(Meat meat){
+    @PostMapping(value = "/add_Meat")
+    public String addMeat(@RequestParam("Meatid") Integer Meatid,
+                          @RequestParam("Mname") String Mname,
+                          @RequestParam("Mstyle") String Mstyle,
+                          @RequestParam("Mprice") Double Mprice,
+                          @RequestParam("Mpicture") MultipartFile Mpicture) {
+        if (Mpicture.isEmpty()) {
+            return "图片不能为空!";
+        }
+        String fileName = Mpicture.getOriginalFilename();
+        String filepath = "C:/Users/Administrator/IdeaProjects/salefood/src/main/resources/static/images/";
+        File dest = new File(filepath+fileName);
+        String picture;
+        try {
+            Mpicture.transferTo(dest);
+            picture =  filepath +fileName;
+       //     logger.info("上传成功");
+        } catch (IOException e) {
+    //        logger.error("上传失败");
+            e.printStackTrace();
+            return "上传失败";
+        }
+        Meat meat = new Meat(0, Meatid, Mname, Mstyle, Mprice,0.0, picture);
+        meat.setMpicture("/img/"+fileName);
         meatService.insertMeat(meat);
         return "RstManager_main";
     }
 
 
-    //修改的预处理，通过菜品编号Mid获取当前记录
-    @RequestMapping(value="meat_update",method = RequestMethod.GET)
-    public String updateMeatPre(Integer Mid,Model model){
+    //修改菜品的预处理，通过菜品编号Mid获取当前记录
+    @RequestMapping(value = "meat_update", method = RequestMethod.GET)
+    public String updateMeatPre(Integer Mid, Model model) {
         Meat meat = meatService.selectMeatById(Mid);
-        model.addAttribute("meat",meat);
+        model.addAttribute("meat", meat);
         return "update_Meat";//进入修改页面
     }
     //实际的修改
-    @RequestMapping(value="/update_Meat",method = RequestMethod.POST)
-    public String updateMeat(Meat meat){
+    @PostMapping(value = "/update_Meat")
+    public String updateMeat(@RequestParam("Mid") Integer Mid,
+                             @RequestParam("Meatid") Integer Meatid,
+                             @RequestParam("Mname") String Mname,
+                             @RequestParam("Mstyle") String Mstyle,
+                             @RequestParam("Mprice") Double Mprice,
+                             @RequestParam("Mscore") Double Mscore,
+                             @RequestParam("Mpicture") MultipartFile Mpicture) {
+        if (Mpicture.isEmpty()) {
+            return "图片不能为空!";
+        }
+        String fileName1 = Mpicture.getOriginalFilename();
+        String filepath1 = "C:/Users/Administrator/IdeaProjects/salefood/src/main/resources/static/images/";
+        File dest = new File(filepath1+fileName1);
+        String update_picture;
+        try {
+            Mpicture.transferTo(dest);
+            update_picture =  filepath1 +fileName1;
+            //     logger.info("上传成功");
+        } catch (IOException e) {
+            //        logger.error("上传失败");
+            e.printStackTrace();
+            return "上传失败";
+        }
+        Meat meat = new Meat(Mid,Meatid, Mname, Mstyle, Mprice, Mscore,update_picture);
+        meat.setMpicture("/img/"+fileName1);
         meatService.updateMeatById(meat);
         return "RstManager_main";
     }
 
+
     //Meat表，删除完成后 需要重新获取删除后的新的数据，再显示
     @RequestMapping("/meat_delete")
-    public String deleteMeat(Integer Mid){
+    public String deleteMeat(Integer Mid) {
         meatService.deleteMeatById(Mid);
         return "RstManager_main";
     }
