9532939439caa79a197bb56dfbdc6c6ed330864b
IMTRL <2691943504@qq.com>
2020-01-01 02:29:33 +0800

    0.0.1

--- src/main/resources/static/js/buyfood.js
+++ src/main/resources/static/js/buyfood.js
@@ -1,21 +1,25 @@
+var result
 function changeCZ(fid) {
-    var str  = {"id":fid}
+    var str = {"id": fid}
     $.ajax({
-        url:'/changeCZ',
-        type:'post',
-        data:str,
-        success:function (res) {
+        url: '/changeCZ',
+        type: 'post',
+        data: str,
+        success: function (res) {
+            document.getElementById("foodli").innerHTML = "";
             document.getElementById("meatli").innerHTML = "";
+            result=res
             for (var i = 0; i < res.length; i++) {
-
                 var a = document.createElement("button");
-                a.setAttribute("onclick","#")
-                var node = document.createTextNode("link");
+                a.setAttribute("id","meat"+i)
+                a.setAttribute("value",i)
+                var node = document.createTextNode(res[i].mname);
                 a.appendChild(node)
-                document.getElementById("meatli")
+                document.getElementById("foodli")
                     .appendChild(a)
-
-                document.getElementById("meatli")
+                var b=document.getElementById("meat"+i)
+                b.setAttribute("onclick","changeCP(this.value)")
+                document.getElementById("foodli")
                     .append(document.createElement("br"))
 
             }
@@ -23,28 +27,31 @@ function changeCZ(fid) {
     })
 }
 
-function changeCP(fid) {
-    var str  = {"id":fid}
-    $.ajax({
-        url:'/changeCZ',
-        type:'post',
-        data:str,
-        success:function (res) {
-            document.getElementById("meatli").innerHTML = "";
-            for (var i = 0; i < res.length; i++) {
+function changeCP(i) {
 
-                var a = document.createElement("button");
-                a.setAttribute("onclick","#")
-                var node = document.createTextNode("link");
-                a.appendChild(node)
-                document.getElementById("meatli")
-                    .appendChild(a)
+    document.getElementById("meatli").innerHTML = "";
 
-                document.getElementById("meatli")
-                    .append(document.createElement("br"))
+    var b = document.createElement("p");
+    var nodea = document.createTextNode("风味："+result[i].mstyle+"   每份" + result[i].mprice + "元"+"    评分："+result[i].mscore);
+    b.appendChild(nodea)
+    document.getElementById("meatli")
+        .appendChild(b)
 
-            }
-        }
-    })
+    var c = document.createElement("img");
+    c.setAttribute("src", result[i].mpicture)
+    document.getElementById("meatli")
+        .appendChild(c)
+
+    var c = document.createElement("button");
+    c.setAttribute("onclick", "#")
+    var nodeb = document.createTextNode("加入购物车");
+    c.appendChild(nodeb)
+    document.getElementById("meatli")
+        .appendChild(c)
+
+    document.getElementById("meatli")
+        .append(document.createElement("br"))
+
+
 }
 
